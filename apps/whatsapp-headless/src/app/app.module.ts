810e19f999c6117deba5c69375ef7efbda1d0844
Keyur Shah <keyurshah3939@gmail.com>
2023-03-03 13:31:35 +0530

    Just staretd

--- apps/whatsapp-headless/src/app/app.module.ts
+++ apps/whatsapp-headless/src/app/app.module.ts
@@ -1,11 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { environment } from '../environments/env';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UUIDDefinition } from 'graphql-scalars';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug:environment.production,
+      playground:environment.production,
+      autoSchemaFile: true,
+      typeDefs: [UUIDDefinition],
+      introspection: true,
+      installSubscriptionHandlers: true,
+    }),
+  ],
+  // controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
